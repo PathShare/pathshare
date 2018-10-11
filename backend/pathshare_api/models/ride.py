eb53bccdd63bbc9f82488128b3081e8f31dbc7a8
Fox <fox.bolduc@ttu.edu>
2018-10-11 17:42:40 -0500

    Marshamallow model first attempt

---
+++ backend/pathshare_api/models/ride.py
@@ -0,0 +1,9 @@
+from marshamallow import Schema, fields
+from datetime import datetime
+
+class Ride(Schema):
+    riders = fields.List(fields.Integer()) #Can change Integer to whatever Atlas uses for IDs
+    departure_date = fields.DateTime()
+    departure_location = fields.Dict(keys=fields.Str(), values=fields.Float()) #Ex: {"lat": 35.000241, "long": -106.346231}
+    price_per_seat = fields.Integer()
+    is_active = fields.Boolean()
