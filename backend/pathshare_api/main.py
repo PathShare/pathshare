e0ab8ab9431688b3d61b6b6ae0fbde23f37c4b43
Simon <B30BD351371C686298D32281B337E8@gmail.com>
2018-10-15 23:21:37 -0500

    Remove attempt at global database
    Add get and post route associations

--- backend/pathshare_api/main.py
+++ backend/pathshare_api/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Main module for the server of Pathshare's backend"""
+"""Main module for the server of Pathshare's backend."""
 
 import asyncio
 import os
@@ -9,8 +9,9 @@ from aiohttp import web
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from pathshare_api.controllers import MongoConnection
-from pathshare_api.api import GetEndpoints
+from pathshare_api.api import GetEndpoints, PostEndpoints
 
+    
 async def init_app() -> web.Application:
     """
     Initializes web application.
@@ -26,19 +27,26 @@ async def init_app() -> web.Application:
     uri = f"mongodb+srv://{USERNAME}:{PASSWORD}@pathshare-cluster0-j1oyz.mongodb.net/main"
     client = AsyncIOMotorClient(uri).main
     
-    # Initialize application and associate database
+    # Initialize application and database
     app = web.Application()
-    app["db"] = MongoConnection(client)
+    db = MongoConnection(client)
+    
+    # Initialize endpoint classes
+    # gets = GetEndpoints(db)
+    posts = PostEndpoints(db)
     
     # Add routes to application
-    # Get routes are currently not implemented
-    
     """
+    # GET routes are currently not implemented
     app.router.add_get("/get/ride", GetEndpoints.get_ride)
     app.router.add_get("/get/user", GetEndpoints.get_user)
     app.router.add_get("/get/ride/all", GetEndpoints.get_all_rides)
     app.router.add_get("/get/validation/", GetEndpoints.get_validation)
     """
+
+    # POST routes
+    app.router.add_post("/post/user/new", posts.post_new_user)
+    app.router.add_post("/post/ride/new", posts.post_new_ride)
     return app
 
 
