2158d1d51903ff4c6c4920824791d17e41cfef7a
Simon <B30BD351371C686298D32281B337E8@gmail.com>
2018-10-15 23:15:08 -0500

    Add is_duplicate_email coroutine

--- backend/pathshare_api/controllers/db.py
+++ backend/pathshare_api/controllers/db.py
@@ -29,3 +29,28 @@ class MongoConnection:
 
         yield from self.client.users.insert_one(data).inserted_id
 
+
+    async def is_duplicate_email(self, email: str) -> bool:
+        """Make sure that the email does not already exist in the database.
+        Use MongoDB's aggregation pipeline operators to do this quickly.
+        Group by email and count how many of each email exist. If counted result
+        is equal to 1, reject the POST.
+
+        Parameters
+        ----------
+        email : str
+            The email to be checked for.
+        
+        Returns
+        -------
+        bool
+            True if the account already exists. False if it does not exist.
+        """
+        pipeline = [{"$group": {"_id": "$email", "count": {"$sum": 1}}}]
+        seen = False
+        async for document in self.client.users.aggregate(pipeline):
+            if document.get("_id") == email:
+                if document.get("count") == 1:
+                    seen = True
+                    break
+        return seen
