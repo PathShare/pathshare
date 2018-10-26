36e1707b02cf1568cf1894f55bd6783e3c8216ca
scott <scblechman@gmail.com>
2018-10-26 16:41:31 -0500

    SB-Create_Trip_Search - Removed chat icon and implemented back button

--- frontend/pathshare-app/src/app/shared/navbar/navbar.component.ts
+++ frontend/pathshare-app/src/app/shared/navbar/navbar.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  // Page element displays. These will be updated based on routing information.
+  displayPageTitle = true;
+  displayBackButton = false;
+  displayMenuIcons = true;
+
   constructor() { }
 
   ngOnInit() {
