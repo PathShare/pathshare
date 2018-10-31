716aa041341b0c7703dae7366fef8b2bd599a6ee
scott <scblechman@gmail.com>
2018-10-31 16:20:55 -0500

    SB-Create_Trip_Search - Added map points based on rides in API

--- frontend/pathshare-app/src/app/shared/in-memory-data-service.ts
+++ frontend/pathshare-app/src/app/shared/in-memory-data-service.ts
@@ -5,16 +5,16 @@ import { Ride } from './model/ride';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const rides = [
-      { id: 1, departureDate: new Date(), seatPrice: 20, departureLocation: {latitude: 0.0, longitude: 0.0},
-        destinationLocation: {latitude: 0.0, longitude: 0.0}, active: true}
+      { id: 1, departureDate: new Date(), seatPrice: 20, departureLocation: {latitude: 33.5779, longitude: -101.8552},
+        destinationLocation: {latitude: 32.7767, longitude: -96.7970}, active: true}
     ];
     return {rides};
   }
 
   // Overrides the genId method to ensure that a hero always has an id.
-  // If the heroes array is empty,
+  // If the array is empty,
   // the method below returns the initial number (11).
-  // if the heroes array is not empty, the method below returns the highest
+  // if the array is not empty, the method below returns the highest
   // hero id + 1.
   genId(rides: Ride[]): number {
     return rides.length > 0 ? Math.max(...rides.map(hero => hero.id)) + 1 : 1;
--- frontend/pathshare-app/src/app/trip-search/trip-search.component.ts
+++ frontend/pathshare-app/src/app/trip-search/trip-search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Ride } from '../shared/model/ride';
+import { Ride, Coordinate } from '../shared/model/ride';
 import { SearchService } from './search.service';
+
+interface Segment {start: Coordinate; end: Coordinate; }
 
 @Component({
   selector: 'app-trip-search',
@@ -9,16 +11,55 @@ import { SearchService } from './search.service';
 })
 export class TripSearchComponent implements OnInit {
 
-  allTrips: Ride[];
+  // Stores scaled Cartesian coordinates to plot on map.
+  allTripPoints: Coordinate[];
+
+  // Stores segments between start and end points to plot on map.
+  allTripSegments: Segment[];
 
   constructor(private searchService: SearchService) { }
 
   ngOnInit() {
+    this.allTripPoints = [];
+    this.allTripSegments = [];
+
     this.searchService.getAllRides().subscribe(rides => {
       rides.forEach(ride => {
-        console.log(ride.departureLocation);
+        this.allTripPoints.push(ride.departureLocation);
+        this.allTripPoints.push(ride.destinationLocation);
+        this.allTripSegments.push({start: ride.departureLocation, end: ride.destinationLocation});
       });
     });
   }
 
+  /**
+   * @description projects the geographic coordinates of a ride to the map
+   * displayed on the main page.
+   * @param latitude WGS84 format
+   * @param longitude WGS84 format
+   */
+  private coordinateToPosition(latitude: number, longitude: number) {
+    const LEFT_MARGIN = 285;
+    const RIGHT_MARGIN = 810;
+    const TOP_MARGIN = 0;
+    const BOTTOM_MARGIN = 325;
+
+    const LEFT_LONGITUDE = -124.271;
+    const RIGHT_LONGITUDE = -66.897;
+    const TOP_LATITUDE = 48.969;
+    const BOTTOM_LATITUDE = 25.257;
+
+    // How far between our lat/long margins the point lies
+    const latMargin = ((latitude - BOTTOM_LATITUDE) / (TOP_LATITUDE - BOTTOM_LATITUDE));
+    const longMargin = ((longitude - LEFT_LONGITUDE) / (RIGHT_LONGITUDE - LEFT_LONGITUDE));
+
+    return {x: LEFT_MARGIN + ((RIGHT_MARGIN - LEFT_MARGIN) * latMargin),
+            y: BOTTOM_MARGIN - ((BOTTOM_MARGIN - TOP_MARGIN) * longMargin)};
+  }
+
+  private getTripPath(segment: Segment) {
+    const startPosition = this.coordinateToPosition(segment.start.latitude, segment.start.longitude);
+    const endPosition = this.coordinateToPosition(segment.end.latitude, segment.end.longitude);
+    return `m50 50 100 100`;
+  }
 }
