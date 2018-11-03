2653db09be4d150261cb3e845afca5fdff548a48
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-11-03 00:35:01 -0500

    Include destination in Ride schema

--- backend/pathshare_api/models/ride.py
+++ backend/pathshare_api/models/ride.py
@@ -5,5 +5,6 @@ class Ride(Schema):
     riders = fields.List(fields.Integer()) #Can change Integer to whatever Atlas uses for IDs
     departure_date = fields.DateTime()
     departure_location = fields.Dict(keys=fields.Str(), values=fields.Float()) #Ex: {"lat": 35.000241, "long": -106.346231}
+    destination = fields.Dict(keys=fields.Str(), values=fields.Float())
     price_per_seat = fields.Integer()
     is_active = fields.Boolean()
