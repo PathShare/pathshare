eb0d049b4e26ab071a2bf68d0459e8266f6cce8b
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-11-04 22:51:40 -0600

    Add get_user and get_all_rides routes

--- backend/pathshare_api/main.py
+++ backend/pathshare_api/main.py
@@ -32,17 +32,15 @@ async def init_app() -> web.Application:
     db = MongoConnection(client)
     
     # Initialize endpoint classes
-    # gets = GetEndpoints(db)
+    gets = GetEndpoints(db)
     posts = PostEndpoints(db)
     
     # Add routes to application
-    """
     # GET routes are currently not implemented
-    app.router.add_get("/get/ride", GetEndpoints.get_ride)
-    app.router.add_get("/get/user", GetEndpoints.get_user)
-    app.router.add_get("/get/ride/all", GetEndpoints.get_all_rides)
-    app.router.add_get("/get/validation/", GetEndpoints.get_validation)
-    """
+    # app.router.add_get("/get/ride", GetEndpoints.get_ride)
+    app.router.add_get("/get/user", gets.get_user)
+    app.router.add_get("/get/ride/all", gets.get_all_rides)
+    # app.router.add_get("/get/validation/", GetEndpoints.get_validation)
 
     # POST routes
     app.router.add_post("/post/user/new", posts.post_new_user)
