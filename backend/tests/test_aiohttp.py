0bc0a3498234b82a5001a1349ab0f92a0a4e5c12
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-11-08 02:49:22 -0600

    Complete coverage on /post/user/new
    This also covers the entire database class

--- backend/tests/test_aiohttp.py
+++ backend/tests/test_aiohttp.py
@@ -8,6 +8,8 @@ from json import dumps
 
 import pytest
 
+from bson.objectid import ObjectId
+
 from pathshare_api.main import init_app
 
 
@@ -19,8 +21,8 @@ async def test_new_user(aiohttp_client, loop):
 	https://docs.aiohttp.org/en/stable/web_reference.html#response-classes
 	"""
 
-	# Create an instance of the application
-	app = await init_app()
+	# Create an instance of the application and a connection to the database
+	app, db = await init_app()
 
 	# Create a new, injected aiohttp_client fixture using the app
 	client = await aiohttp_client(app)
@@ -48,4 +50,24 @@ async def test_new_user(aiohttp_client, loop):
 	assert resp.status == 200
 	result = await resp.json()
 	assert "success" in result.keys()
-	assert result["success"] == "Account successfully created."
+	assert "Account successfully created" in result["success"] 
+	user_id = result["success"].split(": ")[1].strip(".") # Keep the user_id for later
+
+	# Do it again and expect an error since the email is not unique
+	resp = await client.post("/post/user/new", data=dumps(data), headers=headers)
+	assert resp.status == 417
+	result = await resp.json()
+	assert "error" in result.keys()
+	assert result["error"] == f"Email {data.get('email')} already has associated account."
+
+	# Once more, but forget a key
+	data.pop("age")
+	resp = await client.post("/post/user/new", data=dumps(data), headers=headers)
+	assert resp.status == 422
+	result = await resp.json()
+	assert "error" in result.keys()
+	assert result["error"] == "Request must contain key: 'age'."
+
+	# Remove the test document from the database, ensure pymongo.results.DeleteResult returns ack
+	deletion_result = await db.client.users.delete_one({"_id": ObjectId(user_id)})
+	assert deletion_result.acknowledged
