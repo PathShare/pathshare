531b7a066b588048ac603cd53b4a3608358c3bc5
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-11-09 01:26:52 -0600

    Add get_ride method
    Include examples for GET endpoints

--- backend/pathshare_api/api/get_endpoints.py
+++ backend/pathshare_api/api/get_endpoints.py
@@ -15,10 +15,17 @@ class GetEndpoints(object):
     ----------
     db : MongoConnection
         An instance of MongoConnection.
+
+    Notes
+    -----
+    Example get_ride call: localhost:5002/get/ride?dest=Houston
+    Example get_user call: localhost:5002/get/user?id=5be52a64dfd3e35fac9fc298
+    Example get_all_rides call: localhost:5002/get/ride/all
+    Example get_validation: This should not be called directly. Only linked via Email verification.
     """
     def __init__(self, db):
         self.db = db
-    
+
 
     async def home(self, request: aiohttp.web_request.Request) -> web.json_response:
         """Default home route for completeness."""
@@ -28,6 +35,39 @@ class GetEndpoints(object):
             return web.json_response({"data": f"Hello from the PathShare API - {host}:{port}."}, status=200)
         return web.json_response({"data": "Hello from the PathShare API."}, status=200)
 
+
+    async def get_ride(self, request: aiohttp.web_request.Request) -> web.json_response:
+        """Get the data of a single ride.
+        
+        Parameters
+        -----------
+        aiohttp.web_request.Request
+            A request to the endpoint made by the frontend web client.
+
+        Returns
+        --------
+        aiohttp.web.json_response
+        Two types of JSON responses.
+            The first occurs when an argument is missing (dest or date). (Status 417 => Expectation Failed)
+            The second occurs when a query is successful. (Status 200 => OK)
+        """
+        dest = request.rel_url.query.get("dest", None)
+        if dest is None:
+            return web.json_response({"error": "Please provide a destination as a request argument (key=dest)."}, status=417)
+        
+        """
+        date = request.rel_url.query.get("date", None)
+        if date is None:
+            return web.json_response({"error": "Please provide a date as a request argument (key=date)."}, status=417)
+        """
+        data = []
+        async for ride in self.db.client.rides.find({"destination": dest}):
+            ride["_id"] = str(ride.get("_id"))
+            data.append(ride)
+        if not data:
+             return web.json_response({"error": f"There are no rides in the database that match destination = {dest}."}, status=404)
+        return web.json_response({"data": data}, status=200)
+    
 
     async def get_user(self, request: aiohttp.web_request.Request) -> web.json_response:
         """Get the data of a single user.
@@ -46,11 +86,11 @@ class GetEndpoints(object):
         """
         _id = request.rel_url.query.get("id", None)
         if _id is None:
-            return web.json_response({"error": "Please provide a user ID as a request argument."}, status=417)
+            return web.json_response({"error": "Please provide a user ID as a request argument (key=id)."}, status=417)
         user_id = ObjectId(_id)
         data = await self.db.client.users.find_one({"_id": user_id})
         if not data:
-            return web.json_response({"error": f"There are no user with ID: {_id}"}, status=417)
+            return web.json_response({"error": f"There are no user with ID: {_id}."}, status=417)
         data.pop("_id") # Remove redundant _id
         data["password"] = await decrypt_password(data.pop("password")) # Update password key with decrypted password
         return web.json_response({"data": data}, status=200)
