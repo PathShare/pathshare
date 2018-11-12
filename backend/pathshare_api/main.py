b93030b1b9b89f4e95d13e4ffead5cd9b9342599
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-11-11 23:33:15 -0600

    Connect patch routes

--- backend/pathshare_api/main.py
+++ backend/pathshare_api/main.py
@@ -10,7 +10,7 @@ from aiohttp import web
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from pathshare_api.controllers import MongoConnection
-from pathshare_api.api import GetEndpoints, PostEndpoints
+from pathshare_api.api import GetEndpoints, PostEndpoints, PatchEndpoints
 
 
 def init_app() -> Tuple[web.Application, AsyncIOMotorClient]:
@@ -21,7 +21,7 @@ def init_app() -> Tuple[web.Application, AsyncIOMotorClient]:
     -------
     Tuple[web.Application, AsyncIOMotorClient]
     aiohttp.web.Application
-         An initialized AIOHTTP web application.
+        An initialized AIOHTTP web application.
     motor.motor_asyncio.AsyncIOMotorClient
         An async connection to the MongoDB Atlas cluster.
     """
@@ -38,7 +38,8 @@ def init_app() -> Tuple[web.Application, AsyncIOMotorClient]:
     # Initialize endpoint classes
     gets = GetEndpoints(db)
     posts = PostEndpoints(db)
-    
+    patches = PatchEndpoints(db)
+
     # Add routes to application
     app.router.add_get("/", gets.home) # Default home
     app.router.add_get("/get/ride", gets.get_ride)
@@ -46,13 +47,17 @@ def init_app() -> Tuple[web.Application, AsyncIOMotorClient]:
     app.router.add_get("/get/ride/all", gets.get_all_rides)
     app.router.add_get("/get/validation", gets.get_validation)
     
-
     # POST routes
     app.router.add_post("/post/user/new", posts.post_new_user)
     app.router.add_post("/post/ride/new", posts.post_new_ride)
+
+    # PATCH routes
+    app.router.add_patch("/patch/ride/{ride_id}/add/rider/{rider_id}", patches.add_rider)
+    app.router.add_patch("/patch/ride/{ride_id}/delete/rider/{rider_id}", patches.remove_rider)
+
     return app, db
 
 
 if __name__ == "__main__":
     app, _ = init_app()
-    web.run_app(app, port=5002)
+    web.run_app(app, port=80)
