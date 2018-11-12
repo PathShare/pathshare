18653246788659eb28c8a2b219b4e36280041d8b
scott <scblechman@gmail.com>
2018-11-12 15:53:55 -0600

    SB-Display_Trip_Listings - Back button automatically displays on navbar

--- frontend/pathshare-app/src/app/shared/navbar/navbar.component.ts
+++ frontend/pathshare-app/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -12,9 +14,32 @@ export class NavbarComponent implements OnInit {
   displayBackButton = false;
   displayMenuIcons = true;
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private router: Router) { }
 
   ngOnInit() {
+    // Check for navigation actions to set navbar element visibilities. If we've
+    // navigated to a new page, check whether we are on the root page or not and
+    // set title and icons accordingly.
+    this.router.events.subscribe((event) => {
+      if (event.constructor.name === 'NavigationStart') {
+        if (event['url'] === '/') {
+          // We are on the root page.
+          this.displayPageTitle = true;
+          this.displayBackButton = false;
+          this.displayMenuIcons = true;
+        } else {
+          this.displayPageTitle = false;
+          this.displayBackButton = true;
+          this.displayMenuIcons = true;
+        }
+      }
+    });
+  }
+
+  navigateBack() {
+    this.location.back();
   }
 
 }
